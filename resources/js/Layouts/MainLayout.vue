b8af7072a74613e94312031351759105e99c5778
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-12 09:46:48 +0800

    addded MainLayout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app v-cloak id="inspire">
+    <left-side-bar />
+    <app-nav-bar />
+    <v-content transition="slide-x-transition" fluid pa-0 ma-0>
+      <slot />
+    </v-content>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import AppFooter from "../partials/AppFooter.vue";
+import AppNavBar from "../partials/AppNavBar.vue";
+import LeftSideBar from "../partials/LeftSideBar.vue";
+
+export default {
+  components: {
+    AppFooter,
+    AppNavBar,
+    LeftSideBar
+  },
+  props: {
+    title: String
+  },
+  mounted() {
+    this.updatePageTitle(this.title);
+  },
+  methods: {
+    updatePageTitle(title) {
+      document.title = title
+        ? `${title} | ${this.$page.app.name}`
+        : this.$page.app.name;
+    }
+  },
+  watch: {
+    title(title) {
+      this.updatePageTitle(title);
+    }
+  }
+};
+</script>
