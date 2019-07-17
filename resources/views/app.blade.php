fb244e975b3df32368f1db692bff42fb77762cd6
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-17 09:03:28 +0800

    no need csrf-token, added some meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,16 +3,14 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
 </head>
-
 <body>
-
-    @inertia
-
+@inertia
 </body>
 
 </html>
