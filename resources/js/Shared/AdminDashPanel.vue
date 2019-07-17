de275fc50db2f4c764a12b792e3a0baf292a47f6
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-17 09:05:43 +0800

    added dashanel for customer and admin

---
+++ resources/js/Shared/AdminDashPanel.vue
@@ -0,0 +1,263 @@
+<template>
+  <v-layout
+    row
+    wrap
+    px-5
+    mx-5
+  >
+    <v-flex xs12>
+      <v-alert
+        :value="true"
+        type="info"
+        class="text-xs-center secondary--text"
+        my-0
+        py-0
+        mx-3
+        px-3
+      >
+        Note: Press Ctrl or CMD + F5 To Do Hard Page Refresh If You Experience Blank Page Unresponsive Due to Old Cached Of Assets (Only In Development Stage)
+      </v-alert>
+    </v-flex>
+    <v-flex
+      :class="{ md6: isSuperAdmin }"
+      xs12
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="brown lighten-3"
+          >
+            local_atm
+          </v-icon>
+          <v-subheader class="white--text">Overall Subscription Commission</v-subheader>
+          {{ stats.total_subscription_commissions }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      v-if="getMe.id === 1"
+      :class="{ md6: isSuperAdmin }"
+      xs12
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="blue lighten-2"
+          >
+            fa-percent
+          </v-icon>
+          <v-subheader class="white--text">Overall Overriding Commission</v-subheader>
+          {{ five_percent }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md4
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px"
+        >
+          <v-icon
+            x-large
+            color="pink lighten-4"
+          >
+            supervised_user_circle
+          </v-icon>
+          <v-subheader class="white--text">Total Users</v-subheader>
+          {{ stats.total_users }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md4
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="cyan lighten-3"
+          >
+            fa-sitemap
+          </v-icon>
+          <v-subheader class="white--text">Total Subscriptions</v-subheader>
+          {{ stats.subscription_total }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md4
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;cursor:pointer;"
+          @click="$router.push({name:'subscriptions'})"
+        >
+          <v-icon
+            x-large
+            color="teal lighten-3"
+          >
+            money
+          </v-icon>
+          <v-subheader class="white--text">Account Subscriptions Count</v-subheader>
+          {{ stats.account_subscriptions }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md4
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;cursor:pointer;"
+          @click="$router.push({name:'account-referrals'})"
+        >
+          <v-icon
+            x-large
+            color="indigo lighten-5"
+          >
+            person_pin
+          </v-icon>
+          <v-subheader class="white--text">Direct Referrals</v-subheader>
+          {{ stats.direct_referrals }}
+          <br>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md4
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="green lighten-2"
+          >
+            fa-money-bill-alt
+          </v-icon>
+          <v-subheader class="white--text">Direct Referral Commission</v-subheader>
+          {{ stats.direct_referral_commission }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md4
+      text-xs-center>
+      <v-card
+        color="tertiary white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="tertiary lighten-5"
+          >
+            attach_money
+          </v-icon>
+          <v-subheader class="white--text">Account Subscription Commission</v-subheader>
+          {{ stats.account_subscription_commission }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    stats: {
+      total_users: 0,
+      subscription_total: 0,
+      account_subscriptions: 0,
+      direct_referrals: 0,
+      total_subscription_commissions: 0,
+      account_subscription_commission: 0,
+      direct_referral_commission: 0,
+      overriding_commission: 0
+    }
+  }),
+  computed: {
+    getMe(){
+      return this.$page.auth.user
+    }, 
+    isSuperAdmin(){
+        if(this.getMe.id <= 1000){
+            return true;
+        }
+        return false;
+    },
+    five_percent(){
+        let subtotal = parseInt(this.stats.total_subscription_commissions) - parseInt(this.stats.account_subscription_commission) - parseInt(this.stats.direct_referral_commission)
+        let percent = subtotal * 0.05
+        if(isNaN(percent)){
+            return 0
+        }else{
+           return  Math.round(percent)
+        }
+    }
+  },
+  mounted() {
+    // this.getStats();
+  },
+  methods: {
+    // getStats() {
+    //   let self = this;
+    //   axios.post(route("api.dashboard.admin.getStats")).then(response => {
+    //       console.log(response.data)
+    //     self.stats = response.data.data;
+    //   });
+    // }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/Shared/CustomerDashPanel.vue
@@ -0,0 +1,136 @@
+<template>
+  <v-layout
+    row
+    wrap
+    pa-5
+    ma-5
+  >
+    <v-flex
+      xs12
+      md6
+      text-xs-center>
+      <v-card
+        color="blue-grey white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;cursor:pointer;"
+          @click=""
+        >
+          <v-icon
+            x-large
+            color="teal lighten-3"
+          >
+            money
+          </v-icon>
+          <v-subheader class="white--text">Total Subscriptions</v-subheader>
+          {{ stats.subscriptions_count }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md6
+      text-xs-center>
+      <v-card
+        color="blue-grey white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;cursor:pointer;"
+          @click=""
+        >
+          <v-icon
+            x-large
+            color="indigo lighten-5"
+          >
+            person_pin
+          </v-icon>
+          <v-subheader class="white--text">Direct Referrals</v-subheader>
+          {{ stats.referrals_count }}
+          <br>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md6
+      text-xs-center>
+      <v-card
+        color="blue-grey white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="blue-grey lighten-5"
+          >
+            attach_money
+          </v-icon>
+          <v-subheader class="white--text">Subscriptions Commission</v-subheader>
+          {{ stats.subscriptions_commission }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex
+      xs12
+      md6
+      text-xs-center>
+      <v-card
+        color="blue-grey white--text"
+        class="ma-1"
+        height="150px">
+        <v-card-text
+          class="title"
+          style="margin-left:auto;margin-right:auto;width:150px;"
+        >
+          <v-icon
+            x-large
+            color="amber lighten-2"
+          >
+            fa-money
+          </v-icon>
+          <v-subheader class="white--text">Referral Commission</v-subheader>
+          {{ stats.referral_commission }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    stats: {
+      subscriptions_count: 1,
+      referrals_count: 1,
+      subscriptions_commission: 1,
+      referral_commission: 1
+    }
+  }),
+  computed: {
+    getMe(){
+      return this.$page.auth.user
+    }
+  },
+  mounted() {
+    // this.getStats();
+  },
+  methods: {
+    // getStats() {
+    //   let self = this;
+      
+    // }
+  }
+};
+</script>
+
+<style>
+</style>
