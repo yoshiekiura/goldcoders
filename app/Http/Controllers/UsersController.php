c93a57a1ec05f0d8d89b24ab892f5fdf8757414e
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-19 18:13:29 +0800

    return all fields in user as props

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,21 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return Inertia::render('User/Index');
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -28,23 +19,12 @@ class UsersController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Remove the specified resource from storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  int                         $id
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function destroy($id)
     {
         //
     }
@@ -52,7 +32,7 @@ class UsersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int                         $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -61,24 +41,73 @@ class UsersController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Display a listing of the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function index()
+    {
+        return Inertia::render('User/Index', [
+            'filters'  => Request::all('sortBy', 'orderBy'),
+            'contacts' => User::orderByName()
+                ->filter(Request::only('sortBy', 'orderBy'))
+                ->paginate()
+                ->transform(function ($user) {
+                    return [
+                        'id'        => $user->id,
+                        'email'     => $user->email,
+                        'username' => $user->username,
+                        'photo_url' => $user->photo_url,
+                        'fname' => $user->fname,
+                        'mname' => $user->mname,
+                        'lname' => $user->lname,
+                        'suffix' => $user->suffix,
+                        'sponsor' => $user->sponsor,
+                        'avatar' => $user->avatar,
+                        'active' => $user->active,
+                        'dob' => $user->dob,
+                        'mobile_no' => $user->mobile_no,
+                        'permanent_address' => $user->permanent_address,
+                        'current_address' => $user->current_address,
+                        'email_verified_at' => $user->email_verified_at,
+                        'roles' => $user->getroles(),
+                        'abilities' => $user->getabilities()
+                    ];
+                })
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int                         $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
     {
         //
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Store a newly created resource in storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request    $request
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request    $request
+     * @param  int                         $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
         //
     }
