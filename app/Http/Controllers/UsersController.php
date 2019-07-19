625a3fa9cb30db4a583fbe0968afd8edf4f6dcc2
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-19 18:26:39 +0800

    format psr2

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -54,28 +54,27 @@ class UsersController extends Controller
                 ->paginate()
                 ->transform(function ($user) {
                     return [
-                        'id'        => $user->id,
-                        'email'     => $user->email,
-                        'username' => $user->username,
-                        'photo_url' => $user->photo_url,
-                        'fname' => $user->fname,
-                        'mname' => $user->mname,
-                        'lname' => $user->lname,
-                        'suffix' => $user->suffix,
-                        'sponsor' => $user->sponsor,
-                        'avatar' => $user->avatar,
-                        'active' => $user->active,
-                        'dob' => $user->dob,
-                        'mobile_no' => $user->mobile_no,
+                        'id'                => $user->id,
+                        'email'             => $user->email,
+                        'username'          => $user->username,
+                        'photo_url'         => $user->photo_url,
+                        'fname'             => $user->fname,
+                        'mname'             => $user->mname,
+                        'lname'             => $user->lname,
+                        'suffix'            => $user->suffix,
+                        'sponsor'           => $user->sponsor,
+                        'avatar'            => $user->avatar,
+                        'active'            => $user->active,
+                        'dob'               => $user->dob,
+                        'mobile_no'         => $user->mobile_no,
                         'permanent_address' => $user->permanent_address,
-                        'current_address' => $user->current_address,
+                        'current_address'   => $user->current_address,
                         'email_verified_at' => $user->email_verified_at,
-                        'roles' => $user->getroles(),
-                        'abilities' => $user->getabilities()
+                        'roles'             => $user->getroles(),
+                        'abilities'         => $user->getabilities()
                     ];
                 })
         ]);
-
     }
 
     /**
