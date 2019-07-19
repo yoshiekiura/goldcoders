3dbf4aaf35deb7ffc4372384e9f2c36ba0efec50
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-19 18:16:46 +0800

    updated config admin

---
+++ config/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'username' => env('ADMIN_USERNAME', 'admin'),
+    'fname'    => env('ADMIN_FNAME', 'super'),
+    'lname'   => env('ADMIN_LNAME', 'admin'),
+    'email'    => env('ADMIN_EMAIL', 'admin@example.com'),
+    'password' => env('ADMIN_PASSWORD', 'secret')
+];
