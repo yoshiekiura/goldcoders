616181421728ae0b6fc9003b3585cba4bcf339c3
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-20 17:53:13 +0800

    add MassDelete but not yet finished with all the subscriptions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Exceptions\AccountCreationFailed;
 
 class UsersController extends Controller
 {
@@ -13,9 +16,44 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $data = request()->validate([
+            'fname'                 => 'required',
+            'mname'                 => 'nullable',
+            'lname'                 => 'required',
+            'suffix'                => 'nullable',
+            'mobile_no'             => 'nullable',
+            'dob'                   => 'nullable',
+            'username'              => 'nullable',
+            'email'                 => 'nullable|email|unique:users',
+            'password'              => 'required|min:6|confirmed',
+            'password_confirmation' => 'required',
+            'roles'                 => [
+                'sometimes',
+                'required',
+                'exists:roles,name'
+            ],
+            'active'                => 'boolean',
+            'current_address'       => 'nullable',
+            'permanent_address'     => 'nullable'
+        ]);
+        DB::beginTransaction();
+        $user  = User::create($data);
+        $roles = request('roles');
+        \Bouncer::sync($user)->roles($roles);
+
+        try {
+            if (!$user) {
+                throw new AccountCreationFailed;
+            }
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['message' => $e->getMessage()], 400); // Failed Creation
+        }
+
+        DB::commit();
+        return redirect()->back();
     }
 
     /**
@@ -48,8 +86,8 @@ class UsersController extends Controller
     public function index()
     {
         return Inertia::render('User/Index', [
-            'filters'  => Request::all('sortBy', 'orderBy'),
-            'contacts' => User::orderByName()
+            'filters' => Request::all('sortBy', 'orderBy'),
+            'users'   => User::orderByName()
                 ->filter(Request::only('sortBy', 'orderBy'))
                 ->paginate()
                 ->transform(function ($user) {
@@ -57,6 +95,7 @@ class UsersController extends Controller
                         'id'                => $user->id,
                         'email'             => $user->email,
                         'username'          => $user->username,
+                        'name'              => $user->name,
                         'photo_url'         => $user->photo_url,
                         'fname'             => $user->fname,
                         'mname'             => $user->mname,
@@ -71,10 +110,69 @@ class UsersController extends Controller
                         'current_address'   => $user->current_address,
                         'email_verified_at' => $user->email_verified_at,
                         'roles'             => $user->getroles(),
-                        'abilities'         => $user->getabilities()
+                        'permissions'       => $user->getabilities(),
+                        // fix subscription to return proper data for subscriptions
+                        'subscriptions'     => []
                     ];
                 })
         ]);
+    }
+        /**
+     * @return mixed
+     */
+    private function selectExceptSuperAdmin()
+    {
+        $ids = request()->input('selected');
+
+        $except = array_filter($ids, function ($id) {
+            return User::find($id)->isAn('admin');
+        });
+
+        if (count($except) > 0) {
+            foreach ($except as $key => $value) {
+                unset($ids[$key]);
+            }
+        }
+
+        return $ids;
+    }
+    /**
+     * @param Request $request
+     */
+    public function massDelete(Request $request)
+    {
+        $ids = $this->selectExceptSuperAdmin();
+
+        DB::beginTransaction();
+        try {
+            // get all users
+            $users = User::whereIn('id', $ids);
+            // remove all sponsorship
+            // User::whereIn('sp_id', $ids)->update(['sp_id' => null]);
+            // get all subscriptions
+            // $subscriptions = Subscription::whereIn('user_id', $ids);
+            // pluck image_url
+            // $image_urls = $subscriptions->pluck('image_url');
+            // remove null value of image_url
+            // $image_urls = array_filter($image_urls->toArray());
+
+// delete all uploads
+            // if (count($image_urls) > 0) {
+            //     Storage::delete($image_urls);
+            // }
+
+            // remove all subscriptions
+            // $subscriptions->delete();
+            // remove all selected users
+            $users->delete();
+            // remove all uploads /files
+        } catch (\Exception $e) {
+            return redirect()->back()->with('errors', 'Failed To Delete Users');
+        }
+
+        DB::commit();
+        return redirect()->back()->with('success', 'Failed To Delete Users');
+
     }
 
     /**
