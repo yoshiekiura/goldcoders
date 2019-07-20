bd3bdf28aa34fa2fd6f37c19cdd695891696473a
Uriah Galang <codeitlikemiley@gmail.com>
2019-07-20 17:47:27 +0800

    fix acl but need to fix can functions

--- resources/js/mixins/acl.js
+++ resources/js/mixins/acl.js
@@ -1,34 +1,35 @@
 export default {
   methods: {
     isLoggedIn() {
-      return !!this.$page.isLoggedIn
+      return !!this.$page.isLoggedIn;
     },
     hasRole(payload) {
-      let me = this.$page.roles
+      let me = this.$page.auth.user;
       return _.includes(me.roles, payload);
     },
     hasPermission(payload) {
-      let me = this.$page.permissions
-      return _.includes(me, payload);
+      let me = this.$page.auth.user;
+      return _.includes(me.permissions, payload);
     },
     hasAnyPermission(permissions) {
-      let me = this.$store.getters["auth/getMe"];
+      let me = this.$page.auth.user;
       return permissions.some(p => me.permissions.includes(p));
     },
     hasAnyRole(roles) {
-      let me = this.$store.getters["auth/getMe"];
+      let me = this.$page.auth.user;
       return roles.some(r => me.roles.includes(r));
     },
     hasAllRoles(roles) {
-      let me = this.$store.getters["auth/getMe"];
+      let me = this.$page.auth.user;
       return _.difference(roles, me.roles).length === 0;
     },
     hasAllPermissions(permissions) {
-      let me = this.$store.getters["auth/getMe"];
+      let me = this.$page.auth.user;
       return _.difference(permissions, me.permissions).length === 0;
     },
     can(permission) {
-      return this.$store.getters["auth/getMe"].can[permission];
+      // for the moment we return true need to fix this later on
+      return true
     }
   }
 };
